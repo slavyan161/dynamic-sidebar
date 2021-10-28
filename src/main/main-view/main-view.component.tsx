70837447ef2f1205a4a30ccd3d42684ad3c59fea
reza6546 <reza6546@gmail.com>
2021-10-28 12:45:19 +0700

    [Add] Main View Component for main content

---
+++ src/main/main-view/main-view.component.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import 'main-view.css';
+
+export interface IMainViewComponent {
+
+}
+
+function MainViewComponent(props: IMainViewComponent) {
+    return (
+        <div>
+
+        </div>
+    )
+}
+
+export default MainViewComponent
